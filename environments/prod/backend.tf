146dda46efdb99cefe71201d58cab52e3153c837
neeraj <neerajbalamurali@gmail.com>
2026-06-26 23:10:49 +0530

    fix(terraform): auto-create state bucket via OIDC role, remove DynamoDB locking, use native S3 locking

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "arogya-tf-state-371454942267"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "arogya-tf-locks"
-    encrypt        = true
+    bucket  = "arogya-tf-state-371454942267"
+    key     = "prod/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile = true
   }
 }
